89ae279941489ce894bd4519224472784b97b565
CHILMEX <ronaldvillarroelp@gmail.com>
2021-03-06 20:20:27 -0300

    Update validaciones.js

--- js/validaciones.js
+++ js/validaciones.js
@@ -52,17 +52,17 @@ function validate() {
   
     if (opcion1.checked && opcion2.checked == true) {
   
-      alert("Estimado/a " + opcion5+ " el valor de Ambos Servicios con IVA incluido es de " + Number(opcion3 + opcion4) * 1.19);
+      alert("Estimado(a) : " + opcion5+ ", " + " el valor de ambos servicios (IVA incluido) es de : " + Number(opcion3 + opcion4) * 1.19);
       return false;
     } else if (opcion1.checked == true) {
   
-      alert("Estimado/a " + opcion5+ " el valor Servicio de " + opcion1.value + " con IVA incluido : " + "$" + opcion3 * 1.19);
+      alert("Estimado(a) : " + opcion5 + ", " + " el valor del servicio de " + opcion1.value + " (IVA incluido) es de : " + "$" + opcion3 * 1.19);
       return false;
     } else if (opcion2.checked == true) {
-      alert("Estimado/a " + opcion5+" el valor Servicio de " + opcion2.value + " con IVA incluido : " + "$" + opcion4 * 1.19);
+      alert("Estimado(a) : " + opcion5 + ", " + " el valor del servicio de " + opcion2.value + " (IVA incluido) es de : " + "$" + opcion4 * 1.19);
       return false;
     } else {
-      alert("Estimado/a " + opcion5+" no ha seleccionado opciones. Seleccione una de ellas o ambas.");
+      alert("Estimado(a) : " + opcion5 + ", " + " no ha seleccionado opciones. Seleccione una de ellas o ambas.");
       return false;
     }
     
